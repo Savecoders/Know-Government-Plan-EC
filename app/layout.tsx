692d01c9b7b11a328a8dc9743d5664d06daa225e
Pablo Pincay Alvarez <72560306+Savecoders@users.noreply.github.com>
2025-03-03 19:15:20 -0500

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from 'next';
+import { ThemeProvider } from '@/components/theme-provider';
+import localFont from 'next/font/local';
+import './globals.css';
+
+const geistSans = localFont({
+  src: './fonts/GeistVF.woff',
+  variable: '--font-geist-sans',
+  weight: '100 900',
+});
+const geistMono = localFont({
+  src: './fonts/GeistMonoVF.woff',
+  variable: '--font-geist-mono',
+  weight: '100 900',
+});
+
+export const metadata: Metadata = {
+  title: 'Title placeholder',
+  description: 'Description placeholder',
+  authors: [{ name: 'Author placeholder', url: 'https://github.com/' }],
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
