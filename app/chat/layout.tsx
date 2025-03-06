aaf369a310d59712c609c081312745bebc3118ab
Savecoders <pablopincay2003@gmail.com>
2025-03-05 20:11:33 -0500

    feat: add ChatLayout and Page components with tooltip support

---
+++ app/chat/layout.tsx
@@ -0,0 +1,15 @@
+import { cn } from '@/lib/utils';
+import { TooltipProvider } from '@/components/ui/tooltip';
+import type { ReactNode } from 'react';
+import { geistMono, geistSans } from '../layout';
+import { ThemeProvider } from '@/components/theme-provider';
+
+export const metadata = {
+  title: 'Assistant Know Proposals Ec 2025',
+  description: 'Assistant Know Proposals Ec 2025',
+  lang: 'en',
+};
+
+export default function ChatLayout({ children }: { children: ReactNode }) {
+  return <TooltipProvider delayDuration={0}>{children}</TooltipProvider>;
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,12 +3,12 @@ import { ThemeProvider } from '@/components/theme-provider';
 import localFont from 'next/font/local';
 import './globals.css';
 
-const geistSans = localFont({
+export const geistSans = localFont({
   src: './fonts/GeistVF.woff',
   variable: '--font-geist-sans',
   weight: '100 900',
 });
-const geistMono = localFont({
+export const geistMono = localFont({
   src: './fonts/GeistMonoVF.woff',
   variable: '--font-geist-mono',
   weight: '100 900',
@@ -26,12 +26,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
+    <>
+      <html lang="en" suppressHydrationWarning>
+        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </>
   );
 }
