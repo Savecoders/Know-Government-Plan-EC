5d68af9263755329fcde46338771b6a9115bc95f
Savecoders <pablopincay2003@gmail.com>
2025-03-07 15:55:38 -0500

    feat: improve question trimming and loading placeholder

--- components/chat-form.tsx
+++ components/chat-form.tsx
@@ -12,6 +12,7 @@ import remarkGfm from 'remark-gfm';
 interface Message {
   role: 'user' | 'assistant';
   content: string;
+  id?: string;
 }
 
 export function ChatForm({ className, ...props }: React.ComponentProps<'form'>) {
@@ -30,9 +31,11 @@ export function ChatForm({ className, ...props }: React.ComponentProps<'form'>) 
     e.preventDefault();
 
     if (!question.trim()) return;
-    const userMessage: Message = { role: 'user', content: question };
+    const questionCopy = `${question.trim()}`;
+    const userMessage: Message = { role: 'user', content: questionCopy };
     setMessages((prev) => [...prev, userMessage]);
     setIsLoading(true);
+    setQuestion('');
 
     // Vibrate when sending a message
     // verify if the browser supports the vibration API
@@ -64,7 +67,6 @@ export function ChatForm({ className, ...props }: React.ComponentProps<'form'>) 
         navigator.vibrate(50);
       }
 
-      setQuestion('');
       setIsLoading(false);
       setHasTyped(false);
     }
@@ -83,24 +85,27 @@ export function ChatForm({ className, ...props }: React.ComponentProps<'form'>) 
 
   return (
     <>
-      <main
-        className={cn(
-          'ring-none mx-auto flex h-svh max-h-svh w-full max-w-[45rem] flex-col items-stretch border-none',
-          className,
-        )}
-        {...props}
-      >
-        <div className="flex-1 content-center overflow-y-auto px-6">
-          {messages.length > 0 ? (
-            <>
-              <div className="my-4 flex h-fit min-h-full flex-col gap-4">
+      <main className={cn('ring-none mx-auto flex h-svh flex-col overflow-hidden', className)} {...props}>
+        <div className="flex-grow overflow-y-scroll scroll-smooth px-6 pt-12 pb-32 [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:bg-zinc-200 dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500 [&::-webkit-scrollbar-track]:bg-zinc-100 dark:[&::-webkit-scrollbar-track]:bg-neutral-700">
+          <div className="mx-auto max-w-3xl space-y-4">
+            {messages.length > 0 ? (
+              <div className="my-8 flex h-fit min-h-full scroll-m-0 flex-col gap-6">
                 {messages.map((message, index) => (
                   <div
                     key={index}
                     data-role={message.role}
-                    className="max-w-[80%] rounded-xl px-3 py-2 text-sm data-[role=assistant]:self-start data-[role=assistant]:bg-gray-100 data-[role=assistant]:text-black data-[role=user]:self-end data-[role=user]:bg-blue-500 data-[role=user]:text-white"
+                    className={cn(
+                      'max-w-[80%] rounded-xl px-3 py-2 text-sm',
+                      message.role === 'assistant'
+                        ? 'assistant self-start leading-relaxed text-gray-900 dark:text-gray-300'
+                        : 'self-end rounded-br-none border border-gray-200 bg-white dark:border-zinc-800 dark:bg-zinc-950 dark:text-white',
+                    )}
                   >
-                    <Markdown remarkPlugins={[remarkGfm]}>{message.content}</Markdown>
+                    {message.role === 'assistant' ? (
+                      <Markdown remarkPlugins={[remarkGfm]}>{message.content}</Markdown>
+                    ) : (
+                      <span>{message.content}</span>
+                    )}
                   </div>
                 ))}
                 {/* loading assistant */}
@@ -110,57 +115,60 @@ export function ChatForm({ className, ...props }: React.ComponentProps<'form'>) 
                   </div>
                 )}
               </div>
-            </>
-          ) : (
-            <header className="m-auto flex max-w-96 flex-col gap-5 text-center">
-              <h1 className="text-2xl leading-none font-semibold tracking-tight">ChatBot Know Proposals Ec - 2025</h1>
-              <p className="text-muted-foreground text-sm">
-                This Ia learned on the basis of the official documents of the{' '}
-                <span className="text-foreground">CNE 2025</span>
-                <span className="text-foreground">Of the political parties in the second round</span>
-              </p>
-              <p className="text-muted-foreground text-sm">
-                Ask questions, summarize and learn about the work plans of each political party.
-              </p>
-            </header>
-          )}
+            ) : (
+              <header className="m-auto flex max-w-96 flex-col gap-5 text-center">
+                <h1 className="text-2xl leading-none font-semibold tracking-tight">ChatBot Know Proposals Ec - 2025</h1>
+                <p className="text-muted-foreground text-sm">
+                  This Ia learned on the basis of the official documents of the{' '}
+                  <span className="text-foreground">CNE 2025</span>
+                  <span className="text-foreground">Of the political parties in the second round</span>
+                </p>
+                <p className="text-muted-foreground text-sm">
+                  Ask questions, summarize and learn about the work plans of each political party.
+                </p>
+              </header>
+            )}
+          </div>
         </div>
-
-        <form
-          onSubmit={handleSubmitQuestion}
-          className="border-input bg-background focus-within:ring-ring/10 focus-within:ring-offset-background relative mx-6 mb-6 flex items-center gap-3 rounded-[16px] border p-5 pr-14 text-sm focus-within:ring-2 focus-within:ring-offset-0 focus-within:outline-none"
-        >
-          <AutoResizeTextarea
-            onKeyDown={handleKeyDown}
-            onChange={handleQuestionChange}
-            value={question}
-            placeholder="Enter a message"
-            className="placeholder:text-muted-foreground max-h-[120px] min-h-[24px] w-full flex-1 bg-transparent focus:outline-none focus-visible:ring-0 focus-visible:ring-offset-0"
-          />
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Button
-                type="submit"
-                variant="outline"
-                size="icon"
-                className={cn(
-                  'absolute right-4 bottom-4 size-6 h-8 w-8 flex-shrink-0 rounded-full border-0 transition-all duration-200',
-                  hasTyped ? 'light:bg-zinc-900 scale-110 dark:bg-zinc-200' : 'bg-gray-200',
-                )}
-                disabled={!question.trim() || isLoading}
-              >
-                <ArrowUpIcon
+        <section className="bg-background fixed right-0 bottom-0 left-0 p-4">
+          <form
+            onSubmit={handleSubmitQuestion}
+            className="border-input bg-background focus-within:ring-ring/10 focus-within:ring-offset-background relative mx-auto my-6 flex max-w-3xl items-center rounded-2xl border p-5 pr-15 text-sm focus-within:ring-2 focus-within:ring-offset-0 focus-within:outline-none"
+          >
+            <AutoResizeTextarea
+              onKeyDown={handleKeyDown}
+              onChange={handleQuestionChange}
+              value={question}
+              placeholder={isLoading ? 'Waiting for response...' : 'Ask Anything'}
+              className="placeholder:text-muted-foreground max-h-[120px] min-h-[24px] w-full flex-1 bg-transparent focus:outline-none focus-visible:ring-0 focus-visible:ring-offset-0"
+            />
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button
+                  type="submit"
+                  variant="outline"
+                  size="icon"
                   className={cn(
-                    'h-4 w-4 transition-colors',
-                    hasTyped ? 'light:text-white dark:text-zinc-950' : 'text-gray-500',
+                    'absolute right-4 bottom-4 size-6 h-8 w-8 flex-shrink-0 rounded-full border-0 transition-all duration-200',
+                    hasTyped ? 'scale-110 bg-zinc-900 dark:bg-zinc-200' : 'bg-gray-200',
                   )}
-                />
-                <span className="sr-only">Submit</span>
-              </Button>
-            </TooltipTrigger>
-            <TooltipContent sideOffset={12}>Submit</TooltipContent>
-          </Tooltip>
-        </form>
+                  disabled={!question.trim() || isLoading}
+                >
+                  <ArrowUpIcon
+                    className={cn(
+                      'h-4 w-4 transition-colors',
+                      hasTyped ? 'text-white dark:text-zinc-950' : 'text-gray-500',
+                    )}
+                  />
+                  <span className="sr-only">Submit</span>
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent className="rounded-lg bg-gray-900 p-2 text-xs text-white" sideOffset={12}>
+                Submit
+              </TooltipContent>
+            </Tooltip>
+          </form>
+        </section>
       </main>
     </>
   );
